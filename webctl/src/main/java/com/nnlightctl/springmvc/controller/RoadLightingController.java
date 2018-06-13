335ec6f9b16b7f982340fabc6ac064d891660dae
cqconelin <18910587228@189.cn>
2018-06-13 13:24:55 +0800

    道路照明模块接口。

--- webctl/src/main/java/com/nnlightctl/springmvc/controller/RoadLightingController.java
+++ webctl/src/main/java/com/nnlightctl/springmvc/controller/RoadLightingController.java
@@ -9,6 +9,7 @@ import com.nnlightctl.vo.GISView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
