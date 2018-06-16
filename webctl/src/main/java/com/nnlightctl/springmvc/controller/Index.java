9bfa2d4c8e1feaedf9dcc4cd4024af156b096177
cqconelin <18910587228@189.cn>
2018-06-16 14:11:51 +0800

    配置文件上传。

--- webctl/src/main/java/com/nnlightctl/springmvc/controller/Index.java
+++ webctl/src/main/java/com/nnlightctl/springmvc/controller/Index.java
@@ -1,10 +1,15 @@
 package com.nnlightctl.springmvc.controller;
 
+import com.nnlightctl.result.JsonResult;
 import com.nnlightctl.server.IndexServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class Index extends BaseController {
@@ -21,5 +26,23 @@ public class Index extends BaseController {
     @RequestMapping("/")
     public String indexPage() {
         return "redirect:/html/index.html";
+    }
+
+    @RequestMapping("/uploadFile")
+    @ResponseBody
+    public String uploadFile(MultipartFile file) {
+        if (file != null) {
+            String filePath = "d:/";
+            filePath += file.getOriginalFilename();
+            try {
+                file.transferTo(new File(filePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return toJson(JsonResult.getSUCCESS());
+        }
+
+        return toJson(JsonResult.getFAILURE());
     }
 }
