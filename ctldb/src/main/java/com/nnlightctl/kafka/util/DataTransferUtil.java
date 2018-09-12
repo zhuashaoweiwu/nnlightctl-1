f60ac57fa8e7f26c242f594accead5a8fb67b9e4
cqconelin <18910587228@189.cn>
2018-09-12 19:21:30 +0800

    Netty优化。

--- ctldb/src/main/java/com/nnlightctl/kafka/util/DataTransferUtil.java
+++ ctldb/src/main/java/com/nnlightctl/kafka/util/DataTransferUtil.java
@@ -18,46 +18,46 @@ public class DataTransferUtil {
         byte[] data = e0CommandData.getData();
 
         //uuid
-        byte[] uuidBytes = new byte[64];
-        System.arraycopy(data, 0, uuidBytes, 0, 64);
+        byte[] uuidBytes = new byte[36];
+        System.arraycopy(data, 0, uuidBytes, 0, 36);
         String uuid = new String(uuidBytes, Charset.forName("UTF-8"));
         lightingVolEleRecord.setUid(uuid);
 
         //温度
-        short temperature = (short) ByteConvert.bytesToUshort(data, 64);
+        short temperature = (short) ByteConvert.bytesToUshort(data, 36);
         lightingVolEleRecord.setTemperature(new BigDecimal(String.valueOf(Arithmetic.divide(temperature, "10"))));
 
         //湿度
-        short dampness = (short) ByteConvert.bytesToUshort(data, 66);
+        short dampness = (short) ByteConvert.bytesToUshort(data, 38);
         lightingVolEleRecord.setDampness(new BigDecimal(String.valueOf(Arithmetic.divide(dampness, "10"))));
 
         //光照
-        short beam = (short) ByteConvert.bytesToUshort(data, 68);
+        short beam = (short) ByteConvert.bytesToUshort(data, 40);
         lightingVolEleRecord.setBeam(new BigDecimal(String.valueOf(beam)));
 
         //电流
-        short unsignedEletric = (short)ByteConvert.bytesToUshort(data, 70);
+        short unsignedEletric = (short)ByteConvert.bytesToUshort(data, 42);
         lightingVolEleRecord.setElectricty(new BigDecimal(String.valueOf(unsignedEletric)));
 
         //电压
-        short unsignedVoltage = (short)ByteConvert.bytesToUshort(data, 72);
+        short unsignedVoltage = (short)ByteConvert.bytesToUshort(data, 44);
         lightingVolEleRecord.setVoltage(new BigDecimal(String.valueOf(unsignedVoltage)));
 
         //信号强度
-        short unSignalIntensity = (short)ByteConvert.bytesToUbyte(data, 74);
+        short unSignalIntensity = (short)ByteConvert.bytesToUbyte(data, 46);
         lightingVolEleRecord.setPersist1(new BigDecimal(String.valueOf(unSignalIntensity)));
 
         //时间
-        short unHour = (short)ByteConvert.bytesToUbyte(data, 75);
-        short unMinute = (short)ByteConvert.bytesToUbyte(data, 76);
-        short unSecond = (short)ByteConvert.bytesToUbyte(data, 77);
+        short unHour = (short)ByteConvert.bytesToUbyte(data, 47);
+        short unMinute = (short)ByteConvert.bytesToUbyte(data, 48);
+        short unSecond = (short)ByteConvert.bytesToUbyte(data, 49);
         LocalDateTime dateTime = LocalDateTime.now().withHour(unHour).withMinute(unMinute).withSecond(unSecond);
         lightingVolEleRecord.setRecordDatetime(DateTimeUtil.fromLocalDateTime(dateTime));
 
         //GPS
-        int gpsByteLength = data.length - 1 - 77;
+        int gpsByteLength = data.length - 1 - 49;
         byte[] gpsByte = new byte[gpsByteLength];
-        System.arraycopy(data, 78, gpsByte, 0, gpsByteLength);
+        System.arraycopy(data, 50, gpsByte, 0, gpsByteLength);
         String gpsStr = new String(gpsByte, Charset.forName("UTF-8"));
         String strLongititude = gpsStr.substring(0, gpsStr.indexOf("N"));
         lightingVolEleRecord.setPersist2(new BigDecimal(Arithmetic.divide(strLongititude, "100")));
