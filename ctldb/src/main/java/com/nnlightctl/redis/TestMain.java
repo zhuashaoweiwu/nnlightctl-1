5c2424e16be1f812e884985ebeca59d931341f98
cqconelin <18910587228@189.cn>
2019-01-14 14:43:00 +0800

    服务端bug修复。

--- ctldb/src/main/java/com/nnlightctl/redis/TestMain.java
+++ ctldb/src/main/java/com/nnlightctl/redis/TestMain.java
@@ -3,12 +3,19 @@ package com.nnlightctl.redis;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
 public class TestMain {
     public static void main(String[] args) {
         ApplicationContext ac =  new ClassPathXmlApplicationContext("classpath:/config/spring-redis.xml");
         RedisClientTemplate redisClient = (RedisClientTemplate)ac.getBean("redisClientTemplate");
-        redisClient.set("a", "abc");
-        System.out.println(redisClient.get("a"));
-        System.out.println("end");
+        System.out.println(redisClient.set("a", "abc"));
+        System.out.println(redisClient.get("b"));
+        Map<String, String> hashMap = redisClient.hgetAll("hash-key");
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+        System.out.println("Redis Test End");
+        System.exit(0);
     }
 }
